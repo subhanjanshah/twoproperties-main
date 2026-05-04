2eecd4170ef3130c1dfc4871cba2c7113fe186db
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2026-05-04 13:01:06 +0300

    fixing errors on the files

--- src/threads/ReadData.java
+++ src/threads/ReadData.java
@@ -1,64 +1,54 @@
 package threads;
 
-
 import java.io.BufferedReader;
-
-
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import data.*;
 
-public class ReadData implements Runnable{
+public class ReadData implements Runnable {
     URL url = null;
-	HttpURLConnection conn = null;
-	InputStreamReader isr = null;
-	BufferedReader br=null;
+    HttpURLConnection conn = null;
+    InputStreamReader isr = null;
+    BufferedReader br = null;
+    String s = null;
 
-	String s=null;
-
-	@Override
-	public void run() {
-		while (Robot.getRun()==1) {
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			try {
-				url = new URL("http://172.20.10.9.14:8080/rest/lego/getvalues");
-				conn = (HttpURLConnection)url.openConnection();
-				InputStream is=null;
-				try {
-					is=conn.getInputStream();
-				}
-				catch (Exception e) {
-		  			System.out.println("Exception conn.getInputSteam()");
-		  			e.printStackTrace();
-		            System.out.println("Cannot get InputStream!");
-				}
-				isr = new InputStreamReader(is);
-	      		br=new BufferedReader(isr);
-				while ((s=br.readLine())!=null){
-					String [] values=s.split("#");
-					Robot.setId(values[0]);
-					Robot.setRun(values[1]);
-					Robot.setSpeed(values[2]);
-					Robot.setTurn(values[3]);
-				}
-				br.close();
-				isr.close();
-				is.close();
-				conn.disconnect();
-			}
-	  		catch(Exception e) {
-	  			e.printStackTrace();
-	            System.out.println("Some problem!");
-	  		}
-		}
-	}
-
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                url = new URL("http://172.20.10.9:8080/rest/lego/getvalues");
+                conn = (HttpURLConnection) url.openConnection();
+                InputStream is = null;
+                try {
+                    is = conn.getInputStream();
+                } catch (Exception e) {
+                    System.out.println("Cannot get InputStream!");
+                    e.printStackTrace();
+                }
+                isr = new InputStreamReader(is);
+                br = new BufferedReader(isr);
+                while ((s = br.readLine()) != null) {
+                    String[] values = s.split("#");
+                    Robot.setId(values[0]);
+                    Robot.setRun(values[1]);
+                    Robot.setSpeed(values[2]);
+                    Robot.setTurn(values[3]);
+                }
+                br.close();
+                isr.close();
+                is.close();
+                conn.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Some problem!");
+            }
+        }
+    }
 }
--- src/threads/RunLego.java
+++ src/threads/RunLego.java
@@ -1,26 +1,27 @@
 package threads;
-
-
 import data.*;
 import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 
-public class RunLego implements Runnable{
+public class RunLego implements Runnable {
     UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
     UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		while (Robot.getRun()==1) {
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			motorA.setPower(Robot.turnRight());
-			motorB.setPower(Robot.turnLeft());
-		}
-	}
-
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (Robot.getRun() == 1) {
+                motorA.setPower(Robot.turnRight());
+                motorB.setPower(Robot.turnLeft());
+            } else {
+                motorA.setPower(0);
+                motorB.setPower(0);
+            }
+        }
+    }
 }
